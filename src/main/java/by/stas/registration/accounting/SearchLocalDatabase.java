1e9c42f0b1ddc44f9132a2bf0ec1de707592f352
Stanislav <stanislavlakotka@gmail.com>
2021-04-20 20:02:37 +0300

    создание GET запроса

--- src/main/java/by/stas/registration/accounting/SearchLocalDatabase.java
+++ src/main/java/by/stas/registration/accounting/SearchLocalDatabase.java
@@ -10,23 +10,40 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 @ManagedBean
 @SessionScoped
 public class SearchLocalDatabase implements Serializable {
     @Getter
     private final Query query = new Query();
-    private final SearchResults searchResults = new SearchResults();
+
 
     @PostConstruct
     private void init() {
         System.out.println("SearchLocalDatabase created");
     }
 
-    public void searchIntoLocalDB()  {
+    public void searchIntoLocalDB() throws UnsupportedEncodingException {
+        String surname = URLEncoder.encode(query.getSurname(), "UTF-8");
+
         RestTemplate restTemplate = new RestTemplate();
+
+        SearchResults[] searchResults = restTemplate.getForObject("https://a.todes.by:13555/data-service-test/api/v1/data?" +
+                "sys_organ=26&identif=" + query.getIdentif() + "&surname=" + surname +
+                "&name=" + query.getName() + "&patronymic=" + query.getPatronymic() + "&doc_series=" + query.getDocSeries() +
+                "&doc_num=" + query.getDocNum(), SearchResults[].class);
+        SearchResults searchResults1 = new SearchResults();
+        if (searchResults != null) {
+            for (SearchResults results : searchResults) {
+                System.out.println(results);
+
+            }
+        }
+
         FacesContext.getCurrentInstance()
-                .addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Инфо", searchResults.getSurname()));
+                .addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Инфо", searchResults1.toString()));
 
     }
 
--- src/main/java/by/stas/registration/accounting/SearchResults.java
+++ src/main/java/by/stas/registration/accounting/SearchResults.java
@@ -1,11 +1,19 @@
 package by.stas.registration.accounting;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchResults {
 
     private String surname;
-
+    private String name;
+    private String sname;
+    private String bdate;
+    private String identif;
+   // private String addressLast;
+    private String ateAddress;
+    private String dsdDateRec;
 
 }
